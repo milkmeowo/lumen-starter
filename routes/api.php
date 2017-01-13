c57ffe7afc596a1514e3971962c8ad5f8fde6b5f
milkmeowo <milkmeowo@gmail.com>
2017-01-13 18:19:43 +0800

    perf: use resource for api

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,15 @@
 <?php
 
+use Dingo\Api\Routing\Router;
+
 $api = app('Dingo\Api\Routing\Router');
 
 // v1 version API
 // choose version add this in header    Accept:application/vnd.lumen.v1+json
 $api->version('v1', [
     'namespace' => 'App\Api\Controllers\V1',
-], function ($api) {
+    //'middleware' => ['cors'],
+], function (Router $api) {
     
     /*
     |------------------------------------------------
@@ -15,28 +18,16 @@ $api->version('v1', [
     */
 
     // 已删除列表
-    $api->get('/users/trashed', ['as' => 'users.trashed', 'uses' => 'UsersController@trashedIndex']);
+    $api->get('users/trashed', ['as' => 'users.trashed', 'uses' => 'UsersController@trashedIndex']);
 
     // 显示已删除 User
-    $api->get('/users/trashed/{id}', ['as' => 'users.trashed.show', 'uses' => 'UsersController@trashedShow']);
+    $api->get('users/trashed/{id}', ['as' => 'users.trashed.show', 'uses' => 'UsersController@trashedShow']);
 
     // 恢复
-    $api->put('/users/{id}/restore', ['as' => 'users.restore', 'uses' => 'UsersController@restore']);
+    $api->put('users/{id}/restore', ['as' => 'users.restore', 'uses' => 'UsersController@restore']);
 
-    // 列表 User
-    $api->get('/users', ['as' => 'users.index', 'uses' => 'UsersController@index']);
-
-    // 创建 User
-    $api->post('/users', ['as' => 'users.store', 'uses' => 'UsersController@store']);
-
-    // 显示 User
-    $api->get('/users/{id}', ['as' => 'users.show', 'uses' => 'UsersController@show']);
-
-    // 更新 User
-    $api->put('/users/{id}', ['as' => 'users.update', 'uses' => 'UsersController@update']);
-
-    // 删除 User
-    $api->delete('/users/{id}', ['as' => 'users.destroy', 'uses' => 'UsersController@destroy']);
+    // 资源列表
+    $api->resource('users', 'UsersController');
 
     //:end-routes:
 });
