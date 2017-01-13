164ad3d27026aca8779769e9cce852c7821dea6d
milkmeowo <milkmeowo@gmail.com>
2017-01-13 20:52:33 +0800

    feat: add cors support

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ $api = app('Dingo\Api\Routing\Router');
 // choose version add this in header    Accept:application/vnd.lumen.v1+json
 $api->version('v1', [
     'namespace' => 'App\Api\Controllers\V1',
-    //'middleware' => ['cors'],
+    'middleware' => ['cors'],
 ], function (Router $api) {
     
     /*
